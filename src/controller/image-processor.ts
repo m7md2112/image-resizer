4e3d482a8b079bc5c3c7c8bd450cc52b267e3690
Muhammad Mustafa <muhamed2112@gmail.com>
2022-11-23 14:19:22 +0200

    create sharp image resizing controller

--- src/controller/image-processor.ts
+++ src/controller/image-processor.ts
@@ -1,6 +1,49 @@
 import { Response, Request } from "express";
+import sharp from "sharp";
+import * as fs from "fs";
+
+sharp.cache({ files: 0 }); //remove cached image
 
 export const imageProcessor = (req: Request, res: Response) => {
-  res.send(`<p>Now processing ${req.query.filename}</p>`);
-  console.log(req.query);
+  const { filename, height, width } = req.query;
+  let isOriginalImageExist = fs.existsSync("./images/image.jpg");
+  let isNewImageExist = fs.existsSync("./images/newImage.jpg");
+
+  function resizing() {
+    res.write(`<p>Now processing ${filename}</p>`);
+    sharp(("./images/" + filename) as string)
+      .resize(Number(width), Number(height))
+      .toFile("./images/" + "newImage.jpg")
+      .then(() => res.write(`<p> <img src=/images/newImage.jpg> </p>`))
+      .catch((e) => res.write(`<p>Warning 1 ${JSON.stringify(e)}</p>`));
+  }
+
+  function readMetaData() {
+    sharp("./images/newImage.jpg")
+      .metadata()
+      .then((imageData) => {
+        if (
+          Number(imageData.width) !== Number(width) ||
+          Number(imageData.height) !== Number(height)
+        ) {
+          resizing();
+        }
+      });
+  }
+
+  if (!isOriginalImageExist) {
+    res.write(`<p>Warning: please put image.jpg in images directory</p>`);
+    res.end();
+    return;
+  }
+
+  if (isOriginalImageExist && !isNewImageExist) {
+    resizing();
+    res.write(`<p> <img src=/images/newImage.jpg> </p>`);
+    res.end();
+  } else {
+    readMetaData();
+    res.write(`<p> <img src=/images/newImage.jpg> </p>`);
+    res.end();
+  }
 };
