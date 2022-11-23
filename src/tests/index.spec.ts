7bc24507e9c0cc79a2aa138d9395f547f327e899
Muhammad Mustafa <muhamed2112@gmail.com>
2022-11-23 22:40:11 +0200

    add test cases

--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -1,1 +1,20 @@
-console.log("Start Testing")
+import supertest from "supertest";
+import { app } from "../server";
+
+const appServer = supertest(app);
+
+describe("endpoint: /api/resize-image", (): void => {
+  it("gets chunked response", async (): Promise<void> => {
+    const response: supertest.Response = await appServer.get(
+      "/api/resize-image?filename=image.jpg&height=200&width=350"
+    );
+    expect(response.header["transfer-encoding"]).toBe("chunked");
+  });
+});
+
+describe("endpoint: /", (): void => {
+  it("gets /", async (): Promise<void> => {
+    const response: supertest.Response = await appServer.get("/");
+    expect(response.status).toBe(200);
+  });
+});
