6533bd35087dca27d30189af75e956b758f1e087
Muhammad Mustafa <muhamed2112@gmail.com>
2022-11-23 22:36:07 +0200

    add check if height and width have 0 value

--- src/middleware/image-data-validator.ts
+++ src/middleware/image-data-validator.ts
@@ -9,10 +9,12 @@ export const imageDataValidator = (
   if (
     (filename as string) !== "" &&
     !Number.isNaN(Number(height)) &&
-    !Number.isNaN(Number(width))
+    !Number.isNaN(Number(width)) && 
+    Number(width) > 0 &&
+    Number(height) > 0
   ) {
     next();
   } else {
-    res.send(`<p>Please provide image filename, height and width</p>`);
+    res.send(`<p>Please provide image filename, height and width. check if height or width less than 1</p>`);
   }
 };
