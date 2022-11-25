497ea8fc8312d0e04af6709d549abddf33003a41
Muhammad Mustafa <muhamed2112@gmail.com>
2022-11-25 22:07:25 +0200

    refactor image processing

--- src/controller/image-processor.ts
+++ src/controller/image-processor.ts
@@ -1,29 +1,31 @@
 import { Response, Request } from "express";
 import { existsSync } from "fs";
-import { doImageResize, imageResize } from "../utils/image-resizer";
+import { imageResize } from "../utils/image-resizer";
 
 export const imageProcessor = (req: Request, res: Response): void => {
-  const { filename, height, width } = req.query;
-  const isResizedImageExist: boolean = existsSync(
-    `./images/resized-${filename as string}`
-  );
+  const { filename, width, height } = req.query;
+
   const isInputImageExist: boolean = existsSync(
     `./images/${filename as string}`
   );
 
-  if (isResizedImageExist) {
-    doImageResize(filename as string, Number(height), Number(width));
-    res.send(`<div><img src=/images/resized-${filename as string}></div>`);
-    return;
-  }
+  const isResizedImageExist: boolean = existsSync(
+    `./images/resized-${Number(width)}x${Number(height)}-${filename as string}`
+  );
 
-  if (isInputImageExist) {
-    imageResize(filename as string, Number(height), Number(width));
-    res.write(`<div><img src=/images/resized-${filename as string}></div>`);
-  } else {
+  if (isInputImageExist && !isResizedImageExist) {
+    imageResize(filename as string, Number(width), Number(height));
+  } else if (!isInputImageExist) {
     res.write(
       `<p>Please make sure image filename is correct or the image is exist in images directory.</p>`
     );
+    return;
   }
+
+  res.write(
+    `<div><img src=/images/resized-${Number(width)}x${Number(height)}-${
+      filename as string
+    }></div>`
+  );
   res.end();
 };
