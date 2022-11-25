9888c6bd47a1b53143ef31761527d86458bdeb5f
Muhammad Mustafa <muhamed2112@gmail.com>
2022-11-25 18:19:43 +0200

    refactor to use image-resizer util

--- src/controller/image-processor.ts
+++ src/controller/image-processor.ts
@@ -1,54 +1,29 @@
 import { Response, Request } from "express";
-import sharp from "sharp";
-import * as fs from "fs";
-
-sharp.cache({ files: 0 }); // remove cached image
+import { existsSync } from "fs";
+import { doImageResize, imageResize } from "../utils/image-resizer";
 
 export const imageProcessor = (req: Request, res: Response): void => {
   const { filename, height, width } = req.query;
-  const isOriginalImageExist: boolean = fs.existsSync("./images/image.jpg");
-  const isNewImageExist: boolean = fs.existsSync("./images/newImage.jpg");
+  const isResizedImageExist: boolean = existsSync(
+    `./images/resized-${filename as string}`
+  );
+  const isInputImageExist: boolean = existsSync(
+    `./images/${filename as string}`
+  );
 
-  function resizing(): void {
-    res.write(`<p>Now processing ${filename as string}</p>`);
-    sharp(`./images/${filename as string}`)
-      .resize(Number(width), Number(height))
-      .toFile("./images/" + "newImage.jpg")
-      .then(() => res.write(`<p> <img src=/images/newImage.jpg> </p>`))
-      .catch((e) => res.write(`<p>Warning 1 ${JSON.stringify(e)}</p>`));
-      res.end()
-  }
-
-  function readMetaData(): void {
-    sharp("./images/newImage.jpg")
-      .metadata()
-      .then((imageData) => {
-        if (
-          Number(imageData.width) !== Number(width) ||
-          Number(imageData.height) !== Number(height)
-        ) {
-          resizing();
-        }
-      })
-      .catch((e) =>{ 
-        res.write(`<p>Warning 2 ${JSON.stringify(e)}</p>`)
-        res.end()
-        console.log(e)});
-  }
-
-  if (!isOriginalImageExist) {
-    res.write(`<p>Warning: please put image.jpg in images directory</p>`);
-    res.end();
+  if (isResizedImageExist) {
+    doImageResize(filename as string, Number(height), Number(width));
+    res.send(`<div><img src=/images/resized-${filename as string}></div>`);
     return;
   }
 
-  if (isOriginalImageExist && !isNewImageExist) {
-    resizing();
-    res.write(`<p> <img src=/images/newImage.jpg> </p>`);
-    res.end();
+  if (isInputImageExist) {
+    imageResize(filename as string, Number(height), Number(width));
+    res.write(`<div><img src=/images/resized-${filename as string}></div>`);
   } else {
-    readMetaData();
-    res.write(`<p> <img src=/images/newImage.jpg> </p>`);
-    res.end();
+    res.write(
+      `<p>Please make sure image filename is correct or the image is exist in images directory.</p>`
+    );
   }
+  res.end();
 };
